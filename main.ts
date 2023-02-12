671292a890487d812ad1f029b5c91bc7cab6834c
yiheyang <yiheyang97@gmail.com>
2023-02-12 17:36:10 +0800

    feat(init version): add image generation

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ import { Configuration, OpenAIApi } from 'openai'
 import nodeCache from 'node-cache'
 import dotenv from 'dotenv'
 import { encode } from 'gpt-3-encoder'
+import https from 'https'
 
 const cache = new nodeCache()
 
@@ -29,7 +30,8 @@ const MAX_TOKEN_LENGTH = 4096
 const MAX_GENERATE_TOKEN_LENGTH = 1024
 const INIT_COMMAND = 'The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.'
 
-async function reply (messageID: string, content: string) {
+async function reply (
+  messageID: string, content: string) {
   return await client.im.message.reply({
     path: {
       message_id: messageID
@@ -43,8 +45,61 @@ async function reply (messageID: string, content: string) {
   })
 }
 
+async function downloadFile (url: string) {
+  return new Promise<Buffer>((resolve, reject) => {
+    https.get(url, function (response) {
+      if (response.statusCode !== 200) {
+        // || response.headers['content-type'] !== 'image/png'
+        reject(new Error('Failed to download image.'))
+      }
+      response.setEncoding('binary')
+      let imageData = ''
+      response.on('data', function (chunk) {
+        imageData += chunk
+      })
+      response.on('end', function () {
+        const buffer = Buffer.from(imageData, 'binary')
+        resolve(buffer)
+      })
+    })
+  })
+
+}
+
+async function replyImage (
+  messageID: string, imageKey: string) {
+  return await client.im.message.reply({
+    path: {
+      message_id: messageID
+    },
+    data: {
+      content: JSON.stringify({
+        'image_key': imageKey
+      }),
+      msg_type: 'image'
+    }
+  })
+}
+
 function getTokenLength (content: string) {
   return encode(content).length
+}
+
+function errorHandler (error: any) {
+  if (error.response) {
+    const errorMessage = `[ERROR:${error.response.status}] ${JSON.stringify(
+      error.response.data)}`
+    console.warn(errorMessage)
+    return errorMessage
+  } else if (error.message) {
+    const errorMessage = `[ERROR] ${error.message}`
+    console.warn(errorMessage)
+    return errorMessage
+  } else {
+    const errorMessage = `[ERROR] LarkGPT is unavailable now. Please try again later.`
+    console.warn(`[ERROR] Unknown error occurred.`)
+    return errorMessage
+  }
 }
 
 async function createCompletion (userID: string, question: string) {
@@ -65,7 +120,6 @@ async function createCompletion (userID: string, question: string) {
     }
 
     const finalPrompt = promptHead + prompt + `Human: ${question}\nAI: `
-    console.debug(finalPrompt)
 
     const result = await openai.createCompletion({
       model: 'text-davinci-003',
@@ -88,20 +142,34 @@ async function createCompletion (userID: string, question: string) {
 
     return answer
   } catch (error: any) {
-    if (error.response) {
-      const errorMessage = `[ERROR:${error.response.status}] ${JSON.stringify(
-        error.response.data)}`
-      console.warn(errorMessage)
-      return errorMessage
-    } else if (error.message) {
-      const errorMessage = `[ERROR] ${error.message}`
-      console.warn(errorMessage)
-      return errorMessage
-    } else {
-      const errorMessage = `[ERROR] LarkGPT is unavailable now. Please try again later.`
-      console.warn(`[ERROR] Unknown error occurred.`)
-      return errorMessage
-    }
+    throw errorHandler(error)
+  }
+}
+
+async function createImage (userID: string, prompt: string) {
+  console.info(`[Lark] Receive from ${userID}: /img ${prompt}`)
+
+  try {
+    const result = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: '256x256'
+    })
+    const url = result.data.data[0].url || ''
+    if (!url) throw new Error('Failed to create image.')
+
+    const imgBuffer = await downloadFile(url)
+    const uploadedImg: { image_key?: string | undefined; } = await client.im.image.create(
+      {
+        data: { image: imgBuffer, image_type: 'message' }
+      })
+    if (!uploadedImg.image_key) throw new Error(
+      'Failed to upload image to Lark.')
+
+    console.info(`[OpenAI] Reply image to ${userID}: ${uploadedImg.image_key}`)
+    return uploadedImg.image_key
+  } catch (error) {
+    throw errorHandler(error)
   }
 }
 
@@ -112,17 +180,27 @@ const eventDispatcher = new lark.EventDispatcher({
     // handle each message only once
     const messageID = data.message.message_id
     if (!!cache.get(`message_id:${messageID}`)) return { code: 0 }
-    cache.set(`message_id:${messageID}`, true, 300)
+    cache.set(`message_id:${messageID}`, true, 3600)
 
     const userID = data.sender.sender_id?.user_id || 'common'
 
     const messageHandler = async (content: string) => {
-      if (content === '/reset') {
-        cache.del(`session:${userID}`)
-        return await reply(messageID, '[COMMAND] Session reset successfully.')
-      } else {
-        const answer = await createCompletion(userID, content)
-        return await reply(messageID, answer)
+      try {
+        if (content === '/reset') {
+          cache.del(`session:${userID}`)
+          return await reply(messageID, '[COMMAND] Session reset successfully.')
+        } else if (content.match(/^\/img \S+/)) {
+          const imageKey = await createImage(userID,
+            content.replace(/^\/img /, ''))
+          return await replyImage(messageID, imageKey)
+        } else {
+          const answer = await createCompletion(userID, content)
+          return await reply(messageID, answer)
+        }
+      } catch (errorMessage) {
+        if (typeof errorMessage === 'string') {
+          return await reply(messageID, errorMessage)
+        }
       }
     }
 
